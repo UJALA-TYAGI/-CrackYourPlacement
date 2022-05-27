7523b9e0645fc8fc7d6353a13e7be0d265046aab
UJALA TYAGI <UJALATYAGI2512@GMAIL.COM>
2022-05-27 17:42:10 +0530

    Create 3Sum.cpp

---
+++ Day 2/3Sum.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        
+        sort(nums.begin(), nums.end());
+        
+        vector<vector<int>> ans;
+        
+        for(int i = 0; i<nums.size(); i++){
+            
+            if (i>0 && nums[i]==nums[i-1])
+                continue;
+            
+            
+            int j = i+1;
+            int k = nums.size()-1;
+            
+            while(j<k){
+                
+                int sum = nums[i] + nums[j] + nums[k];
+                
+                if (sum>0) k--;
+                
+                if(sum<0) j++;
+                
+                if(sum==0){
+                    ans.push_back(vector<int> {nums[i], nums[j], nums[k]});
+                    
+                    while(j<k && nums[j]==nums[j+1]) j++;
+                    while(j<k && nums[k]==nums[k-1]) k--;
+                    
+                    j++;
+                    k--;
+                    
+                    
+                }
+            }
+            
+        }
+        
+        return ans;
+    }
+};
