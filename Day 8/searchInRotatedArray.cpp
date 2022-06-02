323c0086cdc1413aff4ab61136d08b4d91b75248
UJALA TYAGI <UJALATYAGI2512@GMAIL.COM>
2022-06-02 23:24:51 +0530

    Create searchInRotatedArray.cpp

---
+++ Day 8/searchInRotatedArray.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        
+        int low = 0; 
+        int high = nums.size()-1;
+        
+        while(low<=high) {
+            int mid = (low+high)>>1;
+            
+            if(nums[mid] == target) return mid;
+            
+            else if(nums[low]<=nums[mid]){
+                
+                if(nums[low]<=target && nums[mid]>=target){
+                    high = mid-1;
+                }
+                else{
+                    low = mid+1;
+                }
+            }
+            
+            else {
+                
+                if(nums[high]>=target && nums[mid]<=target){
+                    low = mid+1;
+                }
+                else{
+                    high = mid-1;
+                }
+        }
+            
+        }
+        return -1;
+        
+    }
+};
