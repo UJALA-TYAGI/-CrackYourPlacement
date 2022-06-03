25855f47ff85abf6240179f87320137c54923da3
UJALA TYAGI <UJALATYAGI2512@GMAIL.COM>
2022-06-03 07:13:49 +0530

    Create removeDuplicatesFromList.cpp

---
+++ Day 10/removeDuplicatesFromList.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        
+        if(head == NULL){
+            return 0;
+        }
+        if(head->next == NULL){
+            return head;
+        }
+        ListNode* l = head;
+        
+        while(l->next){
+            
+                if(l->val == l->next->val){
+                    l->next = l->next->next;
+                }
+            
+            else{
+                l = l->next;
+            }
+        }
+     return head;   
+    }
+};
